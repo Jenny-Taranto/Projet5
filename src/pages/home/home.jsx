62a41a70452baee8650a47e28b96a2deb7773a60
Jenny Taranto <jennytaranto.pro@gmail.com>
2025-03-13 10:08:19 +0100

    page home terminée

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,17 +2,19 @@ import React from "react"
 import Banner from "../../components/banner/Banner"
 import Card from "../../components/card/Card"
 import lodgings from "../../data/lodging.json"
- 
+
 
 function Home() {
   return (
     <>
-      <Banner />
-      <div className="gallery">
-        {lodgings.map(lodging => (
-          <Card key={lodging.id} image={lodging.cover} title={lodging.title} />
-        ))}
-      </div>
+      <main>
+        <Banner />
+        <div className="gallery">
+          {lodgings.map(lodging => (
+            <Card key={lodging.id} image={lodging.cover} title={lodging.title} />
+          ))}
+        </div>
+      </main>
     </>
   );
 };
