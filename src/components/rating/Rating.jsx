ee7cb9882074e5c533ca117e80c4ae709496b1f2
Jenny Taranto <jennytaranto.pro@gmail.com>
2025-03-18 13:40:15 +0100

    pages et composants terminés

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -1,10 +1,19 @@
-import { NavLink } from 'react-router-dom'
+import React from "react"
 
-function Nav() {
+function Rating({ rating }) {
+    const Star = ({ filled }) => (
+        <img src={filled ? "/Star-filled.png" : "/Star-empty.png"} alt={filled ? "Étoile remplie" : "Étoile vide"} />
+    )
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+        stars.push(<Star key={i} filled={i <= rating} />);
+    }
+
     return (
-<>
-</>
+        <div className="rating">
+            {stars}
+        </div>
     )
 }
 
-export default Nav
+export default Rating
--- src/pages/lodging/lodging.jsx
+++ src/pages/lodging/lodging.jsx
@@ -1,39 +1,52 @@
-import React from "react"
-import { useParams } from "react-router-dom";
-import Slideshow from "../../components/slideshow/Slideshow"
-import Host from "../../components/host/Host"
-import Rating from "../../components/rating/Rating"
-import Tags from "../../components/tags/Tags"
-import Collapse from "../../components/collapse/Collapse"
-import lodgings from "../../data/lodging.json"
+import { useEffect } from "react";
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Slideshow from "../../components/slideshow/Slideshow";
+import Host from "../../components/host/Host";
+import Rating from "../../components/rating/Rating";
+import Tags from "../../components/tags/Tags";
+import Collapse from "../../components/collapse/Collapse";
+import lodgings from "../../data/lodging.json";
 
 function Lodging() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const lodging = lodgings.find((item) => item.id === id);
+
+  useEffect(() => {
+    if (!lodging) {
+      navigate("*");
+    }
+  }, [lodging, navigate]);
+
   if (!lodging) {
-    return <p>Le logement n'a pas été trouvé.</p>;
+    return null;
   }
 
   return (
-    <>
-      <main>
-        <Slideshow pictures={lodging.pictures} />
-        <div className="group">
-          <div className="lodging-title">
-            <h1>{lodging.title}</h1>
-            <p>{lodging.location}</p>
-          </div>
-          <Host host={lodging.host} />
-          <Tags tags={lodging.tags} />
-          <Rating rating={lodging.rating} />
-          <div>
-            <Collapse title="Description" text={lodging.description} />
-            <Collapse title="Equipements" text={lodging.equipments} />
-          </div>
+    <main>
+      <Slideshow pictures={lodging.pictures} />
+      <div className="group title-host">
+        <div className="lodging-title">
+          <h1>{lodging.title}</h1>
+          <p>{lodging.location}</p>
         </div>
-      </main>
-    </>
-  )
+        <Host host={lodging.host} />
+      </div>
+      <div className="group tags-rating">
+        <Tags tags={lodging.tags} />
+        <Rating rating={lodging.rating} />
+      </div>
+      <div className="collapse-section">
+        <div className="collapse-container">
+          <Collapse title="Description" text={lodging.description} />
+        </div>
+        <div className="collapse-container">
+          <Collapse title="Équipements" text={lodging.equipments} />
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Lodging
+export default Lodging;
